390298f1ad564e0772b451b90fa11d807104f8d7
peachosinthewater <106495743+peachosinthewater@users.noreply.github.com>
2023-11-04 16:20:00 -0500

    The FEDTI get method was defined.

--- data_algorithms.py
+++ data_algorithms.py
@@ -6,5 +6,8 @@ def valid_credit_score(credit_score):
 def debt_income_ratio(monthly_debt, gross_income):
     return monthly_debt / gross_income
 
+def get_FEDTI(monthly_mortgage_payment, gross_income):
+    return monthly_mortgage_payment / gross_income
+    
 def get_total_debt(credit_card_payment, car_payment, student_loans_payments, monthly_mortgage_payment):
-    return total_debt = credit_card_payment + car_payment + student_loans_payments + monthly_mortgage_payment
+    return credit_card_payment + car_payment + student_loans_payments + monthly_mortgage_payment
--- run.py
+++ run.py
@@ -1,4 +1,4 @@
-from flask_app import app
+from flask_app import flask_app
 
 if __name__ == '__main__':
     app.run(debug=True)
