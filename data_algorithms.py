ea8e629f6acc32bd20425f304e56c9aa55726942
duyenemily <duyenemily.nguyen@gmail.com>
2023-11-04 18:56:48 -0500

    Updated  and added functions in data_algorithms

--- data_algorithms.py
+++ data_algorithms.py
@@ -3,7 +3,7 @@ def good_credit_score(credit_score):
     if credit_score >= 640 and credit_score <= 850:
         return True
 """
-# Please evaluate if code is necessary.
+# Please evaluate if code is necessary. -> INPUT VALIDATION FOR ALL VALUES AFTER INPUT???
 
 def evaluate_credit_score(credit_score):
     if credit_score >= 640 and credit_score <= 850:
@@ -30,7 +30,15 @@ def get_FEDTI(monthly_mortgage_payment, gross_income):
 def get_total_debt(credit_card_payment, car_payment, student_loans_payments, monthly_mortgage_payment):
     return credit_card_payment + car_payment + student_loans_payments + monthly_mortgage_payment
 
-def evaluate_DTI(DTI):
+def evaluate_LTV(LTV):
+    if LTV < .8:
+        return "GOOD"
+    elif LTV >= .8 and LTV <= .95:
+        return "OKAY"
+    else:
+        return "BAD"
+
+def evaluate_DTI(DTI, monthly_mortgage_payment, total_debt):
     if DTI > .43:
         # These text statements can be replaced with any numbers that we prefer to determine
         # levels of acceptability.
@@ -39,7 +47,10 @@ def evaluate_DTI(DTI):
     elif DTI <= .43 and DTI > .36:
         return "This DTI is acceptable, but too high to be preferred by lenders."
     elif DTI <= .36:
-        return "This DTI is acceptable, however ensure that no more than 28% of this debt is going to servicing a mortgage."
+        if monthly_mortgage_payment / total_debt > .28:
+            return "This DTI is acceptable, however more than 28% of your monthly debt is going to servicing a mortgage which lenders do not prefer."
+        else:
+            return "This DTI is acceptable."
 
 def evaluate_FEDTI(FEDTI):
     # These strings can also be replaced with any sort of input validation necessary.
