08ab40f5a1792310a23b4da2765c98ef69d3fe11
duyenemily <duyenemily.nguyen@gmail.com>
2023-11-04 16:46:48 -0500

    Added get_LTV and PMI_required functions

--- data_algorithms.py
+++ data_algorithms.py
@@ -3,8 +3,15 @@ def good_credit_score(credit_score):
     if credit_score >= 640 and credit_score <= 850:
         return True
 
-def debt_income_ratio(monthly_debt, gross_income):
+def get_LTV(appraised_value, down_payment):
+    return (appraised_value - down_payment) / appraised_value
+
+def PMI_required(LTV):
+    if LTV < 0.8:
+        return True
+
+def get_DTI(monthly_debt, gross_income):
     return monthly_debt / gross_income
 
 def get_total_debt(credit_card_payment, car_payment, student_loans_payments, monthly_mortgage_payment):
-    return total_debt = credit_card_payment + car_payment + student_loans_payments + monthly_mortgage_payment
+    return credit_card_payment + car_payment + student_loans_payments + monthly_mortgage_payment
