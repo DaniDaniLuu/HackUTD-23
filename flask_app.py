77fa5f92e9f4d21ac4e7f451aa7d579ac748b765
duyenemily <duyenemily.nguyen@gmail.com>
2023-11-04 17:55:22 -0500

    Removed a comment

--- flask_app.py
+++ flask_app.py
@@ -1,5 +1,6 @@
 from flask import Flask, render_template
 import data_algorithms
+
 app = Flask(__name__)
 
 @app.route('/')
@@ -7,6 +8,7 @@ def homepage():
     
     output = str(test_input())
     return output
+
 
 def test_input():
     ID = 1
@@ -19,7 +21,6 @@ def test_input():
     loan_amount = 236251.84
     monthly_mortgage_payment = 1127.90
     credit_score = 778
-    # return data_algorithms.debt_income_ratio(monthly_mortgage_payment, gross_monthly_income)
 
     LTV = data_algorithms.get_LTV(appraised_value, down_payment)
     return LTV
