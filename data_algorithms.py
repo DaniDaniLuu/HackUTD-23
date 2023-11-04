76801429225532720faf5669933b34172653aa16
peachosinthewater <106495743+peachosinthewater@users.noreply.github.com>
2023-11-04 17:34:42 -0500

    Created the evaluate_DTI to find DTI viability.

--- data_algorithms.py
+++ data_algorithms.py
@@ -19,4 +19,13 @@ def get_FEDTI(monthly_mortgage_payment, gross_income):
 def get_total_debt(credit_card_payment, car_payment, student_loans_payments, monthly_mortgage_payment):
     return credit_card_payment + car_payment + student_loans_payments + monthly_mortgage_payment
 
-def evaluate_DTI():
+def evaluate_DTI(DTI):
+    if DTI > .43:
+        # These text statements can be replaced with any numbers that we prefer to determine
+        # levels of acceptability.
+        return ("This DTI is not acceptable. Lowering a DTI can involve transferring high interest loans to a "
+                "low interest credit card, but also consider having too many credit cards can negatively impact home buying power.")
+    elif DTI <= .43 and DTI > .36:
+        return "This DTI is acceptable, but too high to be preferred by lenders."
+    elif DTI <= .36:
+        return "This DTI is acceptable, however ensure that no more than 28% of this debt is going to servicing a mortgage."
