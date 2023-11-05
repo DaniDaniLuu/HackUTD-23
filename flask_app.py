b4265689f998767f8d202b3b0fe9147f8ada47a7
DaniMeloni <122004295+DaniDaniLuu@users.noreply.github.com>
2023-11-05 09:01:12 -0600

    Update flask_app.py

--- flask_app.py
+++ flask_app.py
@@ -1,8 +1,10 @@
 from flask import Flask, render_template, request, jsonify
+from flask_cors import CORS  # Import CORS extension to handle CORS issues
 import data_algorithms
 import data_analysis
 
 app = Flask(__name__)
+CORS(app)
 
 @app.route('/calculation', methods=['POST'])
 def calculation():
@@ -10,27 +12,29 @@ def calculation():
     # LTV value, PMI required (True/False), monthly debt value, DTI value, FEDTI value, credit score enum, LTV enum, DTI enum, FEDTI enum, approval status (True/False), total score value
     #    0                  1                        2              3           4              5              6          7          8                   9                       10 
     return_list = []
+    data = request.get_json()
+    print(data)
     
     # This data would have to be passed in from the jsx form
     #ID = 1 (not relevant to calculation)
-    gross_monthly_income = request.form.get("Credit Payment")
-    credit_card_payment = request.form.get("Credit Payment")
-    car_payment = request.form.get("Car Payment")
-    student_loan_payments = request.form.get("Student Loan Payment")
-    appraised_value = request.form.get("House Appraisal")
-    down_payment = request.form.get("Down Payment")
-    loan_amount = request.form.get("Loan Amount")
+    gross_monthly_income = 0
+    credit_card_payment = 0
+    car_payment = 0
+    student_loan_payments = 0
+    appraised_value = 00
+    down_payment =0
+    loan_amount = 0
     LTV = data_algorithms.get_LTV(appraised_value, down_payment)
     return_list.append(LTV) # Index @ 0 = LTV
     # Make sure to access monthly_mortgage_payment after the if statement to get accurate value
-    monthly_mortgage_payment = request.form.get("Monthly Mortgage")
+    monthly_mortgage_payment = 0
     if data_algorithms.PMI_required(LTV):
         monthly_mortgage_payment *= 1.01
         return_list.append(True) 
     else:
         return_list.append(False)
 
-    credit_score = request.form.get("Credit Score")
+    credit_score = 0
 
     # Additional calculations based on input
     monthly_debt = data_algorithms.get_total_debt(credit_card_payment, car_payment, student_loan_payments, monthly_mortgage_payment)
