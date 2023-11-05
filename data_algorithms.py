7796fdff0aab5e5c6886483fb4a2a10eda39673d
duyenemily <duyenemily.nguyen@gmail.com>
2023-11-04 19:50:07 -0500

    Fixed approval_status to use other functions

--- data_algorithms.py
+++ data_algorithms.py
@@ -31,33 +31,32 @@ def evaluate_DTI(DTI, monthly_mortgage_payment, total_debt):
     if DTI > .43:
         # These text statements can be replaced with any numbers that we prefer to determine
         # levels of acceptability.
-        return ("This DTI is not acceptable. Lowering a DTI can involve transferring high interest loans to a "
-                "low interest credit card, but also consider having too many credit cards can negatively impact home buying power.")
+        return "BAD" # This DTI is not acceptable. Lowering a DTI can involve transferring high interest loans to a low interest credit card, but also consider having too many credit cards can negatively impact home buying power.
     elif DTI <= .43 and DTI > .36:
         if monthly_mortgage_payment / total_debt > .28:
-             return "This DTI is acceptable, but too high to be preferred by lenders, also more than 28% of your monthly debt is going to servicing a mortgage which lenders do not prefer."
+             return "OKAY-MD" # This DTI is acceptable, but too high to be preferred by lenders, also more than 28% of your monthly debt is going to servicing a mortgage which lenders do not prefer.
         else:
-            return "This DTI is acceptable, but too high to be preferred by lenders."
+            return "OKAY" # This DTI is acceptable, but too high to be preferred by lenders.
     elif DTI <= .36:
         if monthly_mortgage_payment / total_debt > .28:
-            return "This DTI is acceptable, however more than 28% of your monthly debt is going to servicing a mortgage which lenders do not prefer."
+            return "GOOD-MD" # This DTI is acceptable, however more than 28% of your monthly debt is going to servicing a mortgage which lenders do not prefer.
         else:
-            return "This DTI is acceptable."
+            return "GOOD" # This DTI is acceptable.
 
 def evaluate_FEDTI(FEDTI):
     # These strings can also be replaced with any sort of input validation necessary.
     if FEDTI <= .25:
-        return "Your FEDTI is acceptable."
+        return "GOOD" # Your FEDTI is acceptable.
     elif FEDTI < .25 and FEDTI <= .28:
-        return "Your FEDTI is acceptable, but could be lower."
+        return "OKAY" # Your FEDTI is acceptable, but could be lower.
     else:
-        return "Your FEDTI needs to be lower than 28%."
+        return "BAD" # Your FEDTI needs to be lower than 28%.
     
-def approval_status(credit_score, LTV, DTI, FEDTI):
-    if (credit_score >= 670 and credit_score <= 850) or (credit_score >= 640 and credit_score < 670):
-        if (LTV < .80) or (LTV >= .80 and LTV < .95):
-            if (DTI <= .36) or (DTI > .36 and DTI <= .43):
-                if (FEDTI <= .25) or (FEDTI > .25 and FEDTI <= .28):
+def approval_status(enum_credit_score, enum_LTV, enum_DTI, enum_FEDTI):
+    if enum_credit_score:
+        if enum_LTV != "BAD":
+            if enum_DTI != "BAD":
+                if enum_FEDTI != "BAD":
                     return True #approved
                 else:
                     return False
