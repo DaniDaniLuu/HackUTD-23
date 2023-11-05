806c31e133475fcb929fb92097d29afd14f507b3
Daniel Kim <118289602+danyobosayo@users.noreply.github.com>
2023-11-04 21:54:06 -0500

    Finished implementing data_analysis column updates

--- data_analysis.py
+++ data_analysis.py
@@ -20,41 +20,65 @@ def data_analysis():
 
     # definte function to evaluate LTV condition
     def eval_rows_LTV(row):
-        if row['LTV'] < .8:
+        if (row['LTV'] < .8):
             return "GOOD"
-        elif row['LTV'] >= .8 and row['LTV'] < .9500001:
+        elif (row['LTV'] >= .8 and row['LTV'] < .9500001):
             return "OKAY"
         else:
             return "BAD"
 
+    def eval_rows_DTI(row):
+        if (row['DTI'] > .43):
+            # These text statements can be replaced with any numbers that we prefer to determine
+            # levels of acceptability.
+            return "BAD" # This DTI is not acceptable. Lowering a DTI can involve transferring high interest loans to a low interest credit card, but also consider having too many credit cards can negatively impact home buying power.
+        elif (row['DTI'] <= .43 and row['DTI'] > .36):
+            if (row['MonthlyMortgagePayment'] / (row['CreditCardPayment'] + row['CarPayment'] + row['StudentLoanPayments'] + row['MonthlyMortgagePayment']) > .28):
+                return "OKAY-MD" # This DTI is acceptable, but too high to be preferred by lenders, also more than 28% of your monthly debt is going to servicing a mortgage which lenders do not prefer.
+            else:
+                return "OKAY" # This DTI is acceptable, but too high to be preferred by lenders.
+        elif (row['DTI'] <= .36):
+            if (row['MonthlyMortgagePayment'] / (row['CreditCardPayment'] + row['CarPayment'] + row['StudentLoanPayments'] + row['MonthlyMortgagePayment']) > .28):
+                return "GOOD-MD" # This DTI is acceptable, however more than 28% of your monthly debt is going to servicing a mortgage which lenders do not prefer.
+            else:
+                return "GOOD" # This DTI is acceptable.
+
+    def eval_rows_FEDTI(row):
+          # These strings can also be replaced with any sort of input validation necessary.
+        if (row['FEDTI'] <= .25):
+            return "GOOD" # Your FEDTI is acceptable.
+        elif (row['FEDTI'] < .25 and row['FEDTI'] <= .28):
+            return "OKAY" # Your FEDTI is acceptable, but could be lower.
+        else:
+            return "BAD" # Your FEDTI needs to be lower than 28%.
+
+    def eval_rows_CS(row):
+        if (row['CreditScore'] >= 670 and row['CreditScore'] <= 850):
+            return "GOOD"
+        if (row['CreditScore'] >= 640 and row['CreditScore'] < 670):
+            return "OKAY"
+        if (row['CreditScore'] < 640):
+            return "BAD"
+
+
+
+    # populate 3 new columns to evaluate the conditions of these values.
     df['EVAL_LTV'] = df.apply(eval_rows_LTV, axis=1)
-    #df['Approval'] = 
+    df['EVAL_DTI'] = df.apply(eval_rows_DTI, axis=1)
+    df['EVAL_FEDTI'] = df.apply(eval_rows_FEDTI, axis=1)
+    df['EVAL_CS'] = df.apply(eval_rows_CS, axis=1)
 
+    def approval_rows_status(row):
+        if (row['EVAL_CS'] != "BAD" and row['EVAL_LTV'] != "BAD" and row['EVAL_DTI'] != "BAD" and row['EVAL_FEDTI'] != "BAD"):
+            return "Approved"
+        else:
+            return "Not Approved"
 
-
-
-
+    df['APPROVAL'] = df.apply(approval_rows_status, axis=1)
 
     print(df.iloc[0])
     print(df.iloc[1])
     print(df.iloc[2])
-    print(df.iloc[3])
-    print(df.iloc[4])
-    print(df.iloc[5])
-    print(df.iloc[6])
-    print(df.iloc[7])
 
     
     return 'meow'
-    #df['Approval'] = df.apply(filler_func, axis=0, raw=False, result_type=broadcast, args=())
-
-    #df['negative_credit_score_impact'] = ''
-    #df['negative_credit_card_payment_impact'] = ''
-    #df['negative_mortgage_payment_impact'] = ''
-
-    # super simple, if credit score is lower than a certain value, make true
-    
-
-    # not as simple, if LTV is <80%, make true
-    #df['negative_credit_card_payment_impact'] = np.where(df.['CreditCardPayment'])
-
